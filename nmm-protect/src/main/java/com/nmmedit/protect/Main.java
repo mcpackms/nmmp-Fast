da7fd42a9855ff70cef03316f2817c32b440dea3
YIZHIDIANBI <mcpackms@outlook.com>
2026-09-05 07:24:02 +0800

    解决了Main.java中缺少package声明的问题

--- nmm-protect/src/main/java/com/nmmedit/protect/Main.java
+++ nmm-protect/src/main/java/com/nmmedit/protect/Main.java
@@ -1,28 +1,36 @@
-public static void main(String[] args) throws IOException {
-    String subCommand;
-    String[] newArgs;
+package com.nmmedit.protect;
 
-    if (args.length < 1) {
-        // 默认使用 apk 子命令，且没有额外参数
-        subCommand = "apk";
-        newArgs = new String[0];
-    } else {
-        subCommand = args[0];
-        newArgs = Arrays.copyOfRange(args, 1, args.length);
-    }
+import java.io.IOException;
+import java.util.Arrays;
 
-    switch (subCommand) {
-        case "apk":
-            ApkMain.main(newArgs);
-            break;
-        case "aab":
-            AabMain.main(newArgs);
-            break;
-        case "aar":
-            AarMain.main(newArgs);
-            break;
-        default:
-            System.err.println("Unknown subcommand");
-            System.exit(-1);
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        String subCommand;
+        String[] newArgs;
+
+        if (args.length < 1) {
+            // 默认使用 apk 子命令，且没有额外参数
+            subCommand = "apk";
+            newArgs = new String[0];
+        } else {
+            subCommand = args[0];
+            newArgs = Arrays.copyOfRange(args, 1, args.length);
+        }
+
+        switch (subCommand) {
+            case "apk":
+                ApkMain.main(newArgs);
+                break;
+            case "aab":
+                AabMain.main(newArgs);
+                break;
+            case "aar":
+                AarMain.main(newArgs);
+                break;
+            default:
+                System.err.println("Unknown subcommand");
+                System.exit(-1);
+        }
     }
 }
