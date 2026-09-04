8de846d92fa3bb992f2fbe0c24f3d1ec6748cd87
YIZHIDIANBI <mcpackms@outlook.com>
2026-09-05 00:12:28 +0800

    消除了构建的警告，添加默认使用apk子命令

--- nmm-protect/src/main/java/com/nmmedit/protect/Main.java
+++ nmm-protect/src/main/java/com/nmmedit/protect/Main.java
@@ -1,37 +1,28 @@
-package com.nmmedit.protect;
+public static void main(String[] args) throws IOException {
+    String subCommand;
+    String[] newArgs;
 
-import java.io.IOException;
-import java.util.Arrays;
+    if (args.length < 1) {
+        // 默认使用 apk 子命令，且没有额外参数
+        subCommand = "apk";
+        newArgs = new String[0];
+    } else {
+        subCommand = args[0];
+        newArgs = Arrays.copyOfRange(args, 1, args.length);
+    }
 
-public class Main {
-
-    public static void main(String[] args) throws IOException {
-        if (args.length < 1) {
-            System.err.println("<apk|aab|aar> subCommand");
-            final String[] newArgs = new String[0];
-            System.err.println("apk:");
+    switch (subCommand) {
+        case "apk":
             ApkMain.main(newArgs);
-            System.err.println("aab:");
+            break;
+        case "aab":
             AabMain.main(newArgs);
-            System.err.println("aar:");
+            break;
+        case "aar":
             AarMain.main(newArgs);
+            break;
+        default:
+            System.err.println("Unknown subcommand");
             System.exit(-1);
-            return;
-        }
-        final String[] newArgs = Arrays.copyOfRange(args, 1, args.length);
-        switch (args[0]) {
-            case "apk":
-                ApkMain.main(newArgs);
-                break;
-            case "aab":
-                AabMain.main(newArgs);
-                break;
-            case "aar":
-                AarMain.main(newArgs);
-                break;
-            default:
-                System.err.println("Unknown subcommand");
-                System.exit(-1);
-        }
     }
 }
